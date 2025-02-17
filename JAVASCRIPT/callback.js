a6276401171327893d7a24765d8f0e6730ce2dbf
Gowdham08 <gowdham0811@gmail.com>
2025-02-17 17:33:05 +0530

    day-7 afternoon commits

--- JAVASCRIPT/callback.js
+++ JAVASCRIPT/callback.js
@@ -1,18 +1,22 @@
-function formSubmission(message, cbfun) {
-    if (cbfun()) {
+function formSubmission(message,cbfun)
+{
+    if(cbfun())
+    {
         console.log(message);
     }
-    else {
+    else
+    {
         console.log("Form submission failed");
     }
 }
 
-function formValidation() {
+function formValidation()
+{
     console.log("Form validation is in progress");
     return true;
 }
 
-formSubmission("Form submitted successfully", formValidation); // Form submitted successfully
+formSubmission("Form submitted successfully",formValidation); // Form submitted successfully
 
 
 console.log();
