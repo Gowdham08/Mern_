254939161d2bc7769848f8c5a1d19b51be58f4a9
Gowdham08 <gowdham0811@gmail.com>
2025-02-26 13:39:29 +0530

    day-14 forennoon commit

--- sjitFrontend/src/components/functionalcomponents/Signup.jsx
+++ sjitFrontend/src/components/functionalcomponents/Signup.jsx
@@ -1,42 +1,61 @@
-import React, { useState ,useEffect } from 'react';
-import { Link } from "react-router-dom";
-function Signup(){
-    return(
-        <div>
-        <h1><center>SignUp</center></h1>
-        <center> <form>
-            <table>
-            <tr>
-                <td><label>FirstName</label> </td>
-                <td><input type="text"></input></td>
-            </tr>
-            <tr>
-            <td> <label>LastName</label></td>
-            <td> <input type="text"></input></td>
-            </tr>
-            <tr>
-            </tr>
-            <tr>
-            <td><label>DOB</label></td>
-            <td><input type="date"></input></td>
-            </tr>
-            <tr>
-            <td> <label htmlFor="Gender">Gender: </label></td>
-            <input type="radio" name="gender" value="Male" id="male"></input>Male
-            <input type="radio" name="gender" value="Female" id="female"></input>Female
-            <input type="radio" name="gender" value="Other" id="other"></input>Other
-            </tr>
-            <tr>
-                <button type="submit">Signup</button>
-            </tr>
-            </table>
-        </form>
-        <div>
-        Already having an account? <Link to="/login">Login</Link>
-        </div>
-        </center>
-        </div>
-    )
-}
-export default Signup;
+//import "../css/formstyle.css"
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+function Signup() {
+    const navigate = useNavigate();
+    const [firstName, setFName] = useState("");
+    const [lastName, setLName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPass] = useState("");
+    const [phoneNumber, setPhoneno] = useState(0);
+    const handleSignUp = async (event) => {
+        event.preventDefault()
+        const req = await axios.post("http://localhost:3001/signup", {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber
+        });
+        const message = req.data.message
+        const isSignup = req.data.isSignup
+        if (isSignup) {
+            console.log(isSignup,message)
+            alert(message)
+            navigate('/login')
+        }
+        else {
+            console.log(isSignup,message)
+            alert(message)
+        }
 
+    };
+    return (
+        <div>
+            <div>
+                <h1>Sign In</h1>
+                <form onSubmit={handleSignUp}>
+                    <input type="text" id="firstName" placeholder="First Name" value={firstName} onChange={e => setFName(e.target.value)} />
+                    <br />
+                    <input type="text" id="lastName" placeholder="Last Name" value={lastName} onChange={e => setLName(e.target.value)} />
+                    <br />
+                    <input type="email" id="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+                    <br />
+                    <input type="password" id="password" placeholder="Password" value={password} onChange={e => setPass(e.target.value)} />
+                    <br />
+                    <input type="text" id="phoneNumber" placeholder="Mobile Number" value={phoneNumber} onChange={e => setPhoneno(e.target.value)} />
+                    <br />
+                    <button>Sign In</button>
+                    <br />
+                    <div>
+                        <h3>Already have an account?<Link to='/login'>Login</Link> </h3>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Signup;
